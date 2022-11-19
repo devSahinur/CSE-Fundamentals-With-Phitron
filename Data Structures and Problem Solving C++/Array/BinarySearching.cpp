7219f3d18cfb0d9178b220d586a19a3fa33ec134
Sahinur Islam <infosahinur@gmail.com>
2022-11-20 04:05:23 +0600

    Binary Serching

---
+++ Data Structures and Problem Solving C++/Array/BinarySearching.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+// code complied problem here
+int binarySearch(int array[], int x, int lb, int ub)
+{
+    if (lb <= ub)
+    {
+        int mid = (lb + ub) / 2;
+        // mid== arr[mid]
+        if (x == array[mid])
+        {
+            return mid; // Center
+        }
+        else if (x > array[mid])
+        {
+            binarySearch(array, x, mid + 1, ub); // Right
+        }
+        else
+        {
+            binarySearch(array, x, lb, mid - 1); // Left
+        }
+    }
+    else
+    {
+        return -1;
+    }
+}
+
+int main()
+{
+    int size;
+    cin >> size;
+    int array[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        cin >> array[i];
+    }
+    int checkValue;
+    cout << "Please Enter the value You want to Search: ";
+    cin >> checkValue;
+
+    int indexNumber;
+    indexNumber = binarySearch(array, checkValue, 0, size - 1);
+
+    if (indexNumber != -1)
+    {
+        cout << "Index No: " << indexNumber << " Position No: " << indexNumber + 1 << endl;
+    }
+    else
+    {
+        cout << "Not Found";
+    }
+
+    return 0;
+}
